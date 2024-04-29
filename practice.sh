a2b793d41432aea792181a4d1e839ba2488818c3
Ubuntu <ubuntu@ip-172-31-19-172.ec2.internal>
2024-04-29 16:14:05 +0000

    this is my first commit

---
+++ practice.sh
@@ -0,0 +1,10 @@
+#this command will pull the code from remote repository.
+git pull
+
+#touch command will create a blank file.
+touch test_repo
+
+
+git add .
+git commit -m "this is my first commit"
+git push
